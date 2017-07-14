940bcf846ac5546a1db79de406206d8510026fd8
Satish <satish@ytrre.com>
2017-07-14 17:44:28 +0530

    Source map with uglify

--- src/App.js
+++ src/App.js
@@ -2,4 +2,5 @@ import RootContainer from './RootContainer'
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+
 ReactDOM.render(<RootContainer />, document.getElementById("reactContainer"))
--- webpack.config.js
+++ webpack.config.js
@@ -14,30 +14,26 @@ module.exports = {
         loaders: [{
             test: /\.js$/,
             exclude: /node_modules/,
-            loader: 'babel-loader',
+            loader: 'babel-loader',  //?presets=es2015retainLines=true
             query: {
                 presets: ['react', 'es2015']
             }
         }]
     },
-    devtool:"cheap-eval-source-map",
+    devtool:"cheap-module-eval-source-map",
     plugins: [
-        new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                warnings: true
-            },
-            output: {
-                comments: true
-            },
-        }),
+        new webpack.optimize.UglifyJsPlugin(),
     ]
 }
 
 
 
 /*Working Source map - Sat
-*Try with SourceMapDevToolPlugin or source-map-loader libs
-* eval-source-map
+* source-map  - use it when uglify is disabled
+* eval-source-map - this works with uglify but appends random file map extension
 * cheap-eval-source-map
-* cheap-module-eval-source-map
+* cheap-module-eval-source-map - this looks better with uglify
+*
+*
+* Try with SourceMapDevToolPlugin or source-map-loader libs
 * */
